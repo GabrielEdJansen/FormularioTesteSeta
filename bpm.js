b60facaa26ff6feb7e2fb12a29dfb42668fe6509
GabrielEdJansen <93282987+GabrielEdJansen@users.noreply.github.com>
2022-03-08 10:56:16 -0300

    Update bpm.js

--- bpm.js
+++ bpm.js
@@ -1,3 +1,15 @@
+function onlynumber(evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode( key );
+    //var regex = /^[0-9.,]+$/;
+    var regex = /^[0-9.]+$/;
+    if( !regex.test(key) ) {
+       theEvent.returnValue = false;
+       if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+ }
+
 function preencheCnpjEmp(){
    
     var CodEmp = document.getElementById("selectEmpFil").value
